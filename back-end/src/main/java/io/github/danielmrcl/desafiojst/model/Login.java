8283a3197a30d91c98c669949cbea57969873779
Daniel Marc <danielmarcelo.junior@hotmail.com>
2021-05-30 19:18:20 -0300

    modelo inicial login

---
+++ back-end/src/main/java/io/github/danielmrcl/desafiojst/model/Login.java
@@ -0,0 +1,26 @@
+package io.github.danielmrcl.desafiojst.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "login")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Login {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne(cascade = CascadeType.ALL, optional = false)
+    private Usuario usuario;
+
+    @Column(name = "senha", nullable = false)
+    private String senha;
+}
